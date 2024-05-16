de745ed1c34109518141cd666e31c74e82c52711
sungsik.kim <skystar819@gmail.com>
2024-05-17 08:35:16 +0900

    함수 정의 및 호출 추가

--- app/src/main/java/lox/Interpreter.java
+++ app/src/main/java/lox/Interpreter.java
@@ -1,9 +1,11 @@
 package lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lox.Expr.Assign;
 import lox.Expr.Binary;
+import lox.Expr.Call;
 import lox.Expr.Grouping;
 import lox.Expr.Literal;
 import lox.Expr.Logical;
@@ -11,6 +13,7 @@ import lox.Expr.Unary;
 import lox.Expr.Variable;
 import lox.Stmt.Block;
 import lox.Stmt.Expression;
+import lox.Stmt.Function;
 import lox.Stmt.If;
 import lox.Stmt.Print;
 import lox.Stmt.Var;
@@ -18,7 +21,29 @@ import lox.Stmt.While;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
+  final Environment globals = new Environment();
+
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -74,6 +99,30 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     // 실행되지 않는 코드
     return null;
+  }
+
+  @Override
+  public Object visitCallExpr(Call expr) {
+    Object callee = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren, "Can only call functions and classes");
+    }
+
+    LoxCallable function = (LoxCallable) callee;
+
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " +
+          function.arity() + " arguments but got " +
+          arguments.size() + ".");
+    }
+
+    return function.call(this, arguments);
   }
 
   @Override
@@ -211,6 +260,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Void visitExpressionStmt(Expression stmt) {
     evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitFunctionStmt(Function stmt) {
+    LoxFunction function = new LoxFunction(stmt);
+    environment.define(stmt.name.lexeme, function);
     return null;
   }
 
