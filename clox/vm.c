993df851c97471109f451c4e7f8e19f688c1f446
sungsik.kim <skystar819@gmail.com>
2024-05-24 08:19:43 +0900

    스택 추가

--- clox/vm.c
+++ clox/vm.c
@@ -5,12 +5,27 @@
 
 VM vm;
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+  resetStack();
 
 }
 
 void freeVM() {
 
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
 
 static InterpretResult run() {
@@ -19,6 +34,13 @@ static InterpretResult run() {
 
   for(;;) {
     #ifdef DEBUG_TRACE_EXECUTION
+      printf("      ");
+      for(Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+        printf("[ ");
+        printValue(*slot);
+        printf(" ]");
+      }
+      printf("\n");
       disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
     #endif
 
@@ -26,11 +48,12 @@ static InterpretResult run() {
     switch(instruction = READ_BYTE()) {
       case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
-        printValue(constant);
-        printf("\n");
+        push(constant);
         break;
       }
       case OP_RETURN: {
+        printValue(pop());
+        printf("\n");
         return INTERPRET_OK;
       }
     }
