69815fc53e3e7e9acf0d796d56796884077345c4
sungsik.kim <skystar819@gmail.com>
2024-06-05 14:57:43 +0900

    if, else 분기 추가

--- clox/compiler.c
+++ clox/compiler.c
@@ -123,6 +123,13 @@ static void emitBytes(uint8_t byte1, uint8_t byte2) {
   emitByte(byte2);
 }
 
+static int emitJump(uint8_t instruction) {
+  emitByte(instruction);
+  emitByte(0xff);
+  emitByte(0xff);
+  return currentChunk()->count - 2;
+}
+
 static void emitReturn() {
   emitByte(OP_RETURN);
 }
@@ -139,6 +146,19 @@ static uint8_t makeConstant(Value value) {
 
 static void emitConstant(Value value) {
   emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void patchJump(int offset) {
+  // 점프 오프셋 자체를 보정하기 위해 바이트코드에서 2를 뺀다.
+  int jump = currentChunk()->count - offset - 2;
+
+  if(jump > UINT16_MAX) {
+    error("Too much code to jump over.");
+  }
+
+  currentChunk()->code[offset] = (jump >> 8) & 0xff;
+  currentChunk()->code[offset + 1] = jump & 0xff;
+
 }
 
 static void initCompiler(Compiler* compiler) {
@@ -432,6 +452,24 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void ifStatement() {
+  consume(TOKEN_LEFT_PAREN, "Expect '(' afeter 'if'.");
+  expression();
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+  int thenJump = emitJump(OP_JUMP_IF_FALSE);
+  emitByte(OP_POP);
+  statement();
+
+  int elseJump = emitJump(OP_JUMP);
+
+  patchJump(thenJump);
+  emitByte(OP_POP);
+
+  if(match(TOKEN_ELSE)) statement();
+  patchJump(elseJump);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -475,6 +513,8 @@ static void declaration() {
 static void statement() {
   if(match(TOKEN_PRINT)) {
     printStatement();
+  } else if(match(TOKEN_IF)) {
+    ifStatement();
   } else if (match(TOKEN_LEFT_BRACE)) {
     beginScope();
     block();
