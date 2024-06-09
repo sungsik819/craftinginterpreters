1de09ba5ed4a63b9a7141c906b5683225cec7309
sungsik.kim <skystar819@gmail.com>
2024-06-09 10:09:35 +0900

    for 추가

--- clox/compiler.c
+++ clox/compiler.c
@@ -482,6 +482,51 @@ static void expressionStatement() {
   emitByte(OP_POP);
 }
 
+static void forStatement() {
+  beginScope();
+  consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+  if(match(TOKEN_SEMICOLON)) {
+    // 초기자 없음
+  } else if(match(TOKEN_VAR)) {
+    varDeclaration();
+  } else {
+    expressionStatement();
+  }
+
+  int loopStart = currentChunk()->count;
+  int exitJump = -1;
+  if(!match(TOKEN_SEMICOLON)) {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+    // 조건식이 거짓이면 루프 밖으로 나간다.
+    exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP); // 조건
+  }
+
+  if(!match(TOKEN_RIGHT_PAREN)) {
+    int bodyJump = emitJump(OP_JUMP);
+    int incrementStart = currentChunk()->count;
+    expression();
+    emitByte(OP_POP);
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    emitLoop(loopStart);
+    loopStart = incrementStart;
+    patchJump(bodyJump);
+  }
+
+  statement();
+  emitLoop(loopStart);
+
+  if(exitJump != -1) {
+    patchJump(exitJump);
+    emitByte(OP_POP); // 조건
+  }
+  
+  endScope();
+}
+
 static void ifStatement() {
   consume(TOKEN_LEFT_PAREN, "Expect '(' afeter 'if'.");
   expression();
@@ -558,6 +603,8 @@ static void declaration() {
 static void statement() {
   if(match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_FOR)) {
+    forStatement();
   } else if(match(TOKEN_IF)) {
     ifStatement();
   } else if(match(TOKEN_WHILE)) {
