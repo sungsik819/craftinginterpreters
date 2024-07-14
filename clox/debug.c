69e37671fa987a26f97f4ad0f7d9991c1d4eb9ce
sungsik.kim <skystar819@gmail.com>
2024-07-15 08:48:50 +0900

    클래스 틀 추가

--- clox/debug.c
+++ clox/debug.c
@@ -119,6 +119,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     }
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
+    case OP_CLASS:
+      return constantInstruction("OP_CLASS", chunk, offset);
     default:
       printf("Unknown opcode %d\n", instruction);
       return offset + 1;
--- clox/memory.c
+++ clox/memory.c
@@ -77,6 +77,11 @@ static void blackenObject(Obj* object) {
 #endif
 
   switch(object->type) {
+    case OBJ_CLASS: {
+      ObjClass* klass = (ObjClass*)object;
+      markObject((Obj*)klass->name);
+      break;
+    }
     case OBJ_CLOSURE: {
       ObjClosure* closure = (ObjClosure*)object;
       markObject((Obj*)closure->function);
@@ -106,6 +111,10 @@ static void freeObject(Obj* object) {
 #endif
 
   switch(object->type) {
+    case OBJ_CLASS: {
+      FREE(ObjClass, object);
+      break;
+    }
     case OBJ_CLOSURE: {
       ObjClosure* closure = (ObjClosure*)object;
       FREE_ARRAY(ObjUpvalue*, closure->upvalues, closure->upvalueCount);
